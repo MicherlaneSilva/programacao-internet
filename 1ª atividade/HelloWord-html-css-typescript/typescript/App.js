e5367a19fe2b7450d732149e984d8c8eb4e40b7a
Micherlane Silva <ladyLane@gmail.com>
2021-03-23 13:54:42 -0300

    ;)

---
+++ 1ª atividade/HelloWord-html-css-typescript/typescript/App.js
@@ -0,0 +1,5 @@
+var p = document.createElement('p');
+var saudacao = "Hello World!";
+p.textContent = saudacao;
+document.body.appendChild(p);
+//# sourceMappingURL=App.js.map
---
+++ 1ª atividade/HelloWord-html-css-typescript/typescript/App.ts
@@ -0,0 +1,7 @@
+var p = document.createElement('p');
+
+let saudacao = "Hello World!";
+
+p.textContent = saudacao;
+
+document.body.appendChild(p);
